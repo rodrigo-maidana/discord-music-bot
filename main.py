c0517ef3dfcb473a8008dfd164258fc1fa83b7aa
Rodrigo Maidana <114775631+rodrigo-maidana@users.noreply.github.com>
2023-11-05 21:30:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from music_cog import music_cog
 from help_cog import help_cog
 
-my_secret = "MTEwOTE3MTY4MzE0MDU3NTI0Mg.GS7UdB.dckfs4qeDSiHwBpjmzpFUxEdeqnHPFTVuV6SLc"
+my_secret = "TOKEN"
 
 intents = discord.Intents.default()
 intents.message_content = True
